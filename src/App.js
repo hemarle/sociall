f81f88ccdef523e49a6e9bfa09b77499d1ee85b9
Daramola femi <dfmopeoluwa@gmail.com>
2022-02-08 19:17:31 +0100

    Hero Component added to the App Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Hero from './Hero';
+import homeImage from './images/lady.png'
+
 
 function App() {
     return (
-        <div className="app"></div>
+        <div className="app">
+            <Hero title='Social Media Accounts Instantly' about="Welcome to largest social media account exchange on the planet. Want to take a break permanently on social media or kickstart your influencer career, we've got you covered" optionalText='Buy & Sell' signIn='/login' signUp='/signup'
+                image={homeImage}/>
+        </div>
     );
 }
 
