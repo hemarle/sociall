c41959a4d7ec7dad42194ea45083490abbe6802f
Daramola femi <dfmopeoluwa@gmail.com>
2022-02-09 14:10:18 +0100

    Card component done

---
+++ src/Card.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Card.css'
+function Card({
+    image,
+    title,
+    summary,
+    linkText,
+    link
+}) {
+    return <div className='card'>
+
+        <div className="card__Image">
+            <div className="card__ImageBackground"></div>
+            <img src={image}
+                alt=""/>
+        </div>
+        <div className="card__Content">
+            <h2> {title}</h2>
+            <p> {about}</p>
+            <a href={link}>
+                {linkText}</a>
+        </div>
+    </div>;
+}
+
+export default Card;
