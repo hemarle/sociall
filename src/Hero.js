776b762d0d9cb73820dff8c6705a23ad8ae5e998
Daramola femi <dfmopeoluwa@gmail.com>
2022-02-09 13:10:37 +0100

    Mobile css done

--- src/Hero.js
+++ src/Hero.js
@@ -9,22 +9,27 @@ function Hero({
     optionalText
 }) {
 
-    return <div className='hero'>
-        <div className="hero__Left">
-            <h4> {optionalText}</h4>
-            <h1> {title}</h1>
-            <p> {about}</p>
+    return <div className='hero flex px-[37px] py-[93px] md:p-6'>
+        <div className="hero__Left ">
+            <h4 className='text-sm mb-4'>
+                {optionalText}</h4>
+            <h1 className='font-bold text-2xl leading-10 mb-6 max-w-xs'>
+                {title}</h1>
+            <p className='text-sm leading-6 max-w-sm mb-4 h-28'>
+                {about}</p>
 
-            <div className="hero__Links">
-                <a href={signUp}>
+            <div className="hero__Links flex  justify-between max-w-xs mt-2">
+                <a href={signUp}
+                    className='hero__Link '>
                     Get Started
                 </a>
-                <a href={signIn}>
+                <a href={signIn}
+                    className='hero__Link'>
                     Log in</a>
 
             </div>
         </div>
-        <div className="hero__Right">
+        <div className="hero__Right hidden md:block">
             <img src={image}
                 alt="hero image"/>
         </div>
