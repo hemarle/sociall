ef440118fbcb1aff774eab0a125f6a5605738f06
Daramola femi <dfmopeoluwa@gmail.com>
2022-02-09 15:37:38 +0100

    Card syling for mobile done

--- src/Card.js
+++ src/Card.js
@@ -7,17 +7,23 @@ function Card({
     linkText,
     link
 }) {
-    return <div className='card'>
+    return <div className='card relative mt-[235px] px-5'>
 
-        <div className="card__Image">
-            <div className="card__ImageBackground"></div>
+        <div className="card__Image relative w-[274px] max-w-max mx-auto ">
+            <div className="card__ImageBackground absolute -top-6 -left-6 -z-10"></div>
+
             <img src={image}
-                alt=""/>
+                alt=""
+                className='mb-[27px]'/>
+
         </div>
-        <div className="card__Content">
-            <h2> {title}</h2>
-            <p> {about}</p>
-            <a href={link}>
+        <div className="card__Content ">
+            <h2 className='absolute w-[100vw]  -top-[113px] left-0 text-lg text-center font-medium  '>
+                {title}</h2>
+            <p className='text-center text-[14px] leading-[25px] min-h-[140px] max-w-[353px] mb-[21px] mx-auto'>
+                {about}</p>
+            <a href={link}
+                className='button'>
                 {linkText}</a>
         </div>
     </div>;
