91fe79971d3f2c6eab573a9844408f2b4eda1475
Daramola femi <dfmopeoluwa@gmail.com>
2022-02-10 23:08:02 +0100

    Tutorial styling done

--- src/Tutorial.js
+++ src/Tutorial.js
@@ -3,18 +3,18 @@ import "./Tutorial.css";
 function Tutorial({ title, video }) {
   return (
     <div className="tutorial mt-[113px]">
-      <h2 className="text-center text-[20px] font-medium mb-[36px]">
+      <h2 className="text-center text-[20px] font-medium mb-[36px] lg:mb-[102px] lg:text-[35px]">
         {" "}
         {title}
       </h2>
 
-      <div className="tutorial__Image">
+      <div className="tutorial__Image relative">
         <img
           src={video}
           alt="video link"
-          className="w-[90vw] max-w-[363px] max-h-[227px] mx-auto"
+          className="w-[90vw] max-w-[363px] max-h-[227px] mx-auto lg:max-w-[1025px] max-h-[470px]"
         />
-        <div className="tutorial__Border hidden lg:block"></div>
+        <div className="tutorial__Border hidden lg:block lg:absolute lg:-top-10 lg:left-[65px] lg:w-[1003px] lg:h-[544px]"></div>
       </div>
     </div>
   );
