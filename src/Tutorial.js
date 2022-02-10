acc5f9127b48e05c81ae2e4c18b896b0e1c57bf1
Daramola femi <dfmopeoluwa@gmail.com>
2022-02-10 22:56:20 +0100

    Mobile styling for Tutorial done

--- src/Tutorial.js
+++ src/Tutorial.js
@@ -2,12 +2,19 @@ import React from "react";
 import "./Tutorial.css";
 function Tutorial({ title, video }) {
   return (
-    <div className="tutorial">
-      <h2> {title}</h2>
+    <div className="tutorial mt-[113px]">
+      <h2 className="text-center text-[20px] font-medium mb-[36px]">
+        {" "}
+        {title}
+      </h2>
 
       <div className="tutorial__Image">
-        <img src={video} alt="video link" />
-        <div className="tutorial__Border "></div>
+        <img
+          src={video}
+          alt="video link"
+          className="w-[90vw] max-w-[363px] max-h-[227px] mx-auto"
+        />
+        <div className="tutorial__Border hidden lg:block"></div>
       </div>
     </div>
   );
