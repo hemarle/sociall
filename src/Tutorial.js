c92fd56a95ae48cecdf37834c565fada9136f08c
Daramola femi <dfmopeoluwa@gmail.com>
2022-02-15 19:50:33 +0100

    Tutorial margin bottom added

--- src/Tutorial.js
+++ src/Tutorial.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Tutorial.css";
 function Tutorial({ title, video }) {
   return (
-    <div className="tutorial mt-[113px]">
+    <div className="tutorial my-[113px]">
       <h2 className="text-center text-[20px] font-medium mb-[36px] lg:mb-[102px] lg:text-[35px]">
         {" "}
         {title}
