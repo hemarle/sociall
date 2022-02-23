5d5128fb0ff4c95cbe24ce4437f13beb314fef9f
Daramola femi <dfmopeoluwa@gmail.com>
2022-02-23 17:59:31 +0100

    Testimonial mobile done

--- src/TestimonialCard.js
+++ src/TestimonialCard.js
@@ -2,14 +2,18 @@ import React from "react";
 import "./TestimonialCard.css";
 function TestimonialCard({ testimony, stars, image, author, role }) {
   return (
-    <div className="testimonial__Card px-[20px] py-[47px] h-[392px] text-center ">
-      <p>{testimony}</p>
+    <div className="testimonialCard px-[20px] py-[47px] h-[392px] text-center w-[90vw] max-w-[364px] mx-auto ">
+      <p className="min-h-[121px] mb-[8px]">{testimony}</p>
       <div className="stars">
         {/* <img src={stars} alt="Number of stars" /> */}
       </div>
-      <img src={image} alt="author image" className="h-10 w-10 mx-auto" />
-      <h3>{author}</h3>
-      <h5>{role}</h5>
+      <img
+        src={image}
+        alt="author image"
+        className="h-10 w-10 mx-auto my-[24px]"
+      />
+      <h3 className="testimonialCard__Author font-bold mb-[10px]">{author}</h3>
+      <h5 className="">{role}</h5>
     </div>
   );
 }
