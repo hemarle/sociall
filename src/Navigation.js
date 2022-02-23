3781fa14a9c71a412fbd47d166d8e7b91fc81acf
Daramola femi <dfmopeoluwa@gmail.com>
2022-02-23 21:35:40 +0100

    Navigattion completed

--- src/Navigation.js
+++ src/Navigation.js
@@ -3,21 +3,28 @@ import "./Navigation.css";
 
 function Navigation({ logo, header, links, login, signup }) {
   return (
-    <div className="navigation">
+    <div className="navigation hidden justify-between align-center md:px-6 py-10 md:flex lg:px-[10vw] ">
       <div className="logo">
         <img src={logo} alt="" />
         <h5 className="">{header}</h5>
       </div>
-      <ul className="links">
+      <ul className="links flex justify-around w-[100%] max-w-[400px]">
         {links.map(({ link, linkUrl }) => (
           <li>
             <a href={linkUrl}> {link}</a>
           </li>
         ))}
       </ul>
-      <div className="auth">
-        <a href={login}>Login</a>
-        <a href={signup}>Get Started</a>
+      <div className="auth min-w-max">
+        <a href={login} className="text-red">
+          Login
+        </a>
+        <a
+          href={signup}
+          className="p-2 ml-8 text-red border-2 border-dotted border-red rounded "
+        >
+          Get Started
+        </a>
       </div>
     </div>
   );
