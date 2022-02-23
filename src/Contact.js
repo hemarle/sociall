a92a86b13493bd254eeb64836ba6b3197f79a2a5
Daramola femi <dfmopeoluwa@gmail.com>
2022-02-23 18:40:46 +0100

    Contact us completed

--- src/Contact.js
+++ src/Contact.js
@@ -2,10 +2,14 @@ import React from "react";
 import "./Contact.css";
 function Contact({ title, about, linkText, link }) {
   return (
-    <div classname="contact">
-      <h2 className="title">{title}</h2>
-      <p>{about}</p>
-      <a href={link} accessKey="c">
+    <div className="contact min-h-[160px] py-[76px]">
+      <h2 className="title text-center mb-[34px] md:text-[30px] lg:[35px]">
+        {title}
+      </h2>
+      <p className="max-w-[341px] min-h-[88px] mb-[34px] leading-[26px] text-center mx-auto lg:max-w-[600px]">
+        {about}
+      </p>
+      <a href={link} accessKey="c" className="contact__Link mx-auto ">
         {linkText}
       </a>
     </div>
